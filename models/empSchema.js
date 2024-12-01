f3131f2b26574b180dc6d0ef5d14733b6609af8a
Jorrel6130 <jorrel6130@gmail.com>
2024-12-01 15:26:48 -0500

    Updating schema to make position easier to search

--- models/empSchema.js
+++ models/empSchema.js
@@ -9,7 +9,7 @@ const EmpSchema = new mongoose.Schema({
     first_name: {type: String, required: true, trim: true},
     last_name: {type: String, required: true, trim: true},
     email: {type: String, required: true, trim: true, lowercase: true},
-    position: String,
+    position: {type: String, trim: true, lowercase: true},
     salary: [Number],
     date_of_joining: {type: Date, required: true},
     department: String,
